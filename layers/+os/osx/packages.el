ab40ef9af7897ff6d9d60c077e1a8c25eeea65a0
Christopher Eames (Chream) <chream@gmx.com>
2018-06-17 01:25:47 +0200

    [osx] rm exec-path-from-shell package bc using spacemacs internals.

--- layers/+os/osx/packages.el
+++ layers/+os/osx/packages.el
@@ -11,7 +11,6 @@
 
 (setq osx-packages
       '(
-        exec-path-from-shell
         helm
         launchctl
         (osx-dictionary :toggle osx-use-dictionary-app)
@@ -22,29 +21,6 @@
         reveal-in-osx-finder
         term
         ))
-
-(when (spacemacs/system-is-mac)
-  ;; Enable built-in trash support via finder API if available (only on Emacs
-  ;; Mac Port)
-  (when (boundp 'mac-system-move-file-to-trash-use-finder)
-    (setq mac-system-move-file-to-trash-use-finder t)))
-
-(defun osx/init-exec-path-from-shell ()
-  (use-package exec-path-from-shell
-    :if (spacemacs/system-is-mac)
-    :defer nil
-    :config
-    (progn
-      (exec-path-from-shell-initialize)
-      (when (spacemacs/system-is-mac)
-        ;; Use GNU ls as `gls' from `coreutils' if available.  Add `(setq
-        ;; dired-use-ls-dired nil)' to your config to suppress the Dired warning when
-        ;; not using GNU ls.  We must look for `gls' after `exec-path-from-shell' was
-        ;; initialized to make sure that `gls' is in `exec-path'
-        (let ((gls (executable-find "gls")))
-          (when gls
-            (setq insert-directory-program gls
-                  dired-listing-switches "-aBhl --group-directories-first")))))))
 
 (defun osx/pre-init-helm ()
   ;; Use `mdfind' instead of `locate'.
